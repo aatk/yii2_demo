a0d0e9c2cf8b0c6b3f1197c8900f9d329662e54e
aleksandr <at@zerotech.ru>
2021-02-11 08:08:05 +0300

    [Yii2 demo]
    rest add

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -2,34 +2,227 @@
 
 namespace app\controllers;
 
+use app\models\Users;
+//use App\Repository\UsersRepository;
+//use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Response;
+
 use yii\rest\Controller;
 
 class ApiController extends Controller
 {
-    
     /**
      * {@inheritdoc}
      */
     public function behaviors()
     {
-        return parent::behaviors();
+        $result = parent::behaviors();
+        return $result;
     }
-
+    
     /**
      * {@inheritdoc}
      */
     public function actions()
     {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-        ];
     }
     
     public function actionInstall()
     {
-        $res = ["result"=>"ok"];
-        return  $this->asJson($res);
+        //@Get("/api/install")
+        
+        $maxcount    = 1000;
+        $firstnames  = [ "Александр", "Иван", "Максим", "Олег", "Марат", "Людмила", "Оксана" ];
+        $secondnames = [ "Ткаченко", "Жуков", "Чкалов", "Путин", "Шамузинов", "Дахно", "Карась" ];
+        $surnames    = [ "Александрович", "Иванович", "Максимович", "Олегович", "Маратович", "Евгеньевич", "Джонович" ];
+
+
+        $Users = new Users();
+        $count           = $Users->count();
+
+        $ID = false;
+        if ($count < $maxcount)
+        {
+            for ($next_item = $count; $next_item < $maxcount; $next_item++)
+            {
+                //заполним таблицу
+                $rnd_f  = rand(0, 6);
+                $rnd_s  = rand(0, 6);
+                $rnd_ss = rand(0, 6);
+
+                $Users = new Users();
+                $Users->setFirstname($firstnames[$rnd_f]);
+                $Users->setSecondname($secondnames[$rnd_s]);
+                $Users->setSurname($surnames[$rnd_ss]);
+    
+                $Users->save();
+                $ID = $Users->getId();
+            }
+        }
+
+        $res = [ 'result' => $ID ];
+
+        $result = $this->asJson($res);
+        return $result;
     }
+    
+    public function actionGetid()
+    {
+        $id = $this->request->get("id");
+
+        //@Get("/api/get/{id}")
+        $result = $this->response;
+        $id     = $id ?? 0;
+        if ($id > 0)
+        {
+            $user = Users::findOne($id)->getAttributes();
+            $UserArray       = [];
+            $UserArray[]     = $user;
+            $result = $this->asJson($UserArray);
+        }
+        else
+        {
+            $result->setStatusCode(404);
+        }
+
+        return $result;
+    }
+    
+    public function actionList()
+    {
+        $id    = $this->request->get("id");
+        $limit = $this->request->get("limit");
+
+        // @Get("/api/list/{id}/{limit}")
+        $result = $this->response;
+        $id     = $id ?? 0;
+        $limit  = $limit ?? 1;
+        if ($id > 0)
+        {
+            $Users = Users::find()->where(['>=', 'id', $id])->limit($limit)->all();
+            $result          = $this->asJson($Users);
+        }
+        else
+        {
+            $result->setStatusCode(404);
+        }
+
+        return $result;
+    }
+    
+    public function actionSearch()
+    {
+        $find  = $this->request->get("find");
+        $id    = $this->request->get("id");
+        $limit = $this->request->get("limit");
+
+//        //@Get("/api/search/{find}/{id}/{limit}")
+//        $usersRepository = new UsersRepository($this->getDoctrine());
+//        $findresult      = $usersRepository->findByText($find, $id, $limit);
+//        $result          = $this->json($findresult);
+//
+//        return $result;
+        $res = [ "find" => $find, $limit => $id ];
+        return $this->asJson($res);
+    }
+
+    private function saveUsers($json)
+    {
+        $result = false;
+        
+        if (isset($json["firstname"]) && isset($json["secondname"]) && isset($json["surname"]))
+        {
+            $User = new Users();
+            if (isset($json["id"]))
+            {
+                $User->setId($json["id"]);
+                $User->refresh();
+            }
+            $User->setFirstname($json["firstname"]);
+            $User->setSecondname($json["secondname"]);
+            $User->setSurname($json["surname"]);
+            $User->save();
+            $UserId = $User->getId();
+
+            if ($UserId)
+            {
+                $result = $User;
+            }
+        }
+
+        return $result;
+    }
+
+    public function actionPost()
+    {
+    
+        //@Post("/api/post")
+        $result = $this->response;
+        $result->setStatusCode(404);
+    
+        $content    = $this->request->getRawBody();
+        $json_items = json_decode($content, true);
+        foreach ($json_items as $json)
+        {
+            unset($json["id"]);
+            $UserInfo = $this->saveUsers($json);
+            if ($UserInfo)
+            {
+                $userjson = $UserInfo->getAttributes();
+                $UserInfoArray   = [];
+                $UserInfoArray[] = $userjson;
+                $result          = $this->asJson($UserInfoArray)->setStatusCode(201);
+            }
+        }
+
+        return $result;
+    }
+
+    public function actionPut()
+    {
+        //@Put("/api/put")
+        $result = $this->response;
+        $result->setStatusCode(404);
+
+        $content    = $this->request->getRawBody();
+        $json_items = json_decode($content, true);
+        foreach ($json_items as $json)
+        {
+            $id = $json["id"] ?? 0;
+            if ($id > 0)
+            {
+                $UserInfo = $this->saveUsers($json);
+                if ($UserInfo)
+                {
+                    $result->setStatusCode(201);
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    public function actionDelete()
+    {
+        //@Delete("/api/delete")
+        $result = $this->response;
+        $result->setStatusCode(404);
+    
+        $content    = $this->request->getRawBody();
+        $json_items = json_decode($content, true);
+        foreach ($json_items as $json)
+        {
+            $id = $json["id"] ?? 0;
+            if ($id > 0)
+            {
+                $User = Users::findOne($id);
+                $User->delete();
+                
+                $result->setStatusCode(201);
+            }
+        }
+
+        return $result;
+    }
+
 }
---
+++ models/Users.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+use yii\db\Query;
+
+class Users extends ActiveRecord
+{
+//    public $id = null;
+//    public $firstname = "";
+//    public $secondname = "";
+//    public $surname = "";
+    
+    const STATUS_INACTIVE = 0;
+    const STATUS_ACTIVE = 1;
+    
+    /**
+     * @return string the name of the table associated with this ActiveRecord class.
+     */
+    public static function tableName()
+    {
+        return '{{users}}';
+    }
+    
+    public static function getDb()
+    {
+        return \Yii::$app->db;
+    }
+    
+    
+    public function setFirstname($value)
+    {
+        $this->setAttribute("firstname" , $value);
+    }
+    
+    public function getFirstname()
+    {
+        return $this->getAttribute("firstname");
+    }
+    
+    public function setSecondname($value)
+    {
+        $this->setAttribute("secondname" , $value);
+    }
+    
+    public function getSecondname()
+    {
+        return $this->getAttribute("secondname");
+    }
+    
+    public function setSurname($value)
+    {
+        $this->setAttribute("surname" , $value);
+    }
+    
+    public function getSurname()
+    {
+        return $this->getAttribute("surname");
+    }
+    
+    public function setId($value)
+    {
+        $this->setAttribute("id", $value);
+        return $this;
+    }
+    
+    public function getId()
+    {
+        return $this->getAttribute("id");
+    }
+    
+    
+    public function count()
+    {
+        $count = (new Query())->select('COUNT(*)')->from('users')->count();
+        return $count;
+    }
+    
+}
